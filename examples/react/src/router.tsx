1a7a304bcf497d0890c84cffab7c944b541567fd
Chiko <2452559902@qq.com>
2025-07-30 23:20:52 +0800

    fix: 类型错误

--- examples/react/src/router.tsx
+++ examples/react/src/router.tsx
@@ -1,5 +1,4 @@
-import type { RouteObject } from 'react-router-dom'
 import { createBrowserRouter } from 'react-router-dom'
 import routes from '~react-pages'
 
-export default createBrowserRouter(routes as RouteObject[])
+export default createBrowserRouter(routes)
